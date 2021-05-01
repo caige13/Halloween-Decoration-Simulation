7b439cbcdd4bc9f1ddc15888f9eac879c7cb532d
caige13 <34533619+caige13@users.noreply.github.com>
2021-04-30 22:32:43 -0500

    TestAccum.v made

--- TestAccum.v
+++ TestAccum.v
@@ -1,15 +1,3 @@
-`define ON        '4b0000 //00=system, 00=ON
-`define RESET     '4b0001 //00=system, 01=Reset
-`define GREEN     '4b0100 //01=Color, 00=GREEN
-`define PURPLE    '4b0101 //01=Color, 01=PURPLE
-`define ORANGE    '4b0110 //01=Color, 10=ORANGE
-`define SCREAMING '4b1000 //10=Sound, 00=SCREAMING
-`define CACKLING  '4b1001 //10=Sound, 01=CACKLING
-`define BOO       '4b1010 //10=Sound, 10=BOO
-`define BOO       '4b1010 //10=Sound, 10=BOO
-`define WAVEHANDS '4b1100 //11=Movement/Effect, 00=WAVEHANDS
-`define MOVEJAW   '4b1101 //11=Movement/Effect, 01=MOVEJAW
-`define FOG       '4b1110 //11=Movement/Effect, 10=FOG
 
 module Mux(channels,select, b);
 input [3:0][3:0]channels;
@@ -23,15 +11,19 @@ begin
 end
 endmodule
 
-module DFF(clk,in,out);
+module DFF(rst,clk,in,out);
 	parameter n=2;//width
 	input clk;
+	input rst;
 	input [n-1:0] in;
 	output [n-1:0] out;
 	reg [n-1:0] out;
 
-	always @(posedge clk)
-	out = in;
+	always @(posedge clk or posedge rst)
+	if (rst)  
+        out = 0;  
+    else  
+        out = in; 
 endmodule
 
 //HALF-ADDER
@@ -80,33 +72,29 @@ end
 
 endmodule
 
-module breadboard(Data, A, C);
-input [3:0] [3:0] Data;
-input [1:0] B;
-input [1:0] A;
-//input [1:0] select;
+module breadboard(on, clk, data);
+input clk; 
+input on;
+input [3:0] [3:0] data;
+wire clk;
+wire on;
 wire [3:0][3:0]channels;
-wire [3:0]Data0 = Data[0];
-wire [3:0]Data1 = Data[1];
-wire [3:0]Data2 = Data[2];
-wire [3:0]Data3 = Data[3];
-wire [1:0] A;
-wire [1:0] select;
-wire [1:0] sum;
-wire [3:0] b;
-wire [1:0] S;
-output [1:0] C;
-reg [1:0] C;
+wire [3:0]Data0 = data[0];
+wire [3:0]Data1 = data[1];
+wire [3:0]Data2 = data[2];
+wire [3:0]Data3 = data[3];
+wire [3:0] opcode;
 
-//Registers for ADDER
 reg [1:0] regA;
-wire [1:0] B;
-reg [1:0] next;
-assign B = 2b01;
+reg [1:0] regB=01;
+wire [1:0] outputADD;
 
-ADDER adds(regA, regB, sum, carry);
-DFF accum(clk, next, select);
-Mux mux1(channels,select,b);
+reg  [1:0] next;
+wire [1:0] cur;
+
+ADDER add1(regA,regB,outputADD,carry);
+DFF ACC1 (on,clk,next,cur);
+Mux mux1(channels,cur,opcode);
 
 assign channels[0]=Data0;//OpCode 1
 assign channels[1]=Data1;//OpCode 2
@@ -115,55 +103,49 @@ assign channels[3]=Data3;//OpCode 4
 
 always @(*)
 begin
-	$display("Hello");
-	assign C=b;
-	assign next = sum;
-	regA = A;
+ regA=cur;
+
+ assign next=outputADD;
 end
 endmodule
 
 module testbench();
-	reg clk;
-	//reg [1:0] select;
-	reg [3:0] [3:0] Data;
-	reg [1:0] inputA;
-	wire [1:0] outputC;
-	
-	breadboard bb8(Data, inputA, outputC);
+   reg [1:0] inputA;
+   reg [1:0] inputB;
+   reg [3:0] [3:0] data;
+   reg clk;
+   reg rst;
+   
+	breadboard bb8(rst, clk, data);
 	
 	//CLOCK
    initial begin //Start Clock Thread
      forever //While TRUE
         begin //Do Clock Procedural
           clk=0; //square wave is low
-          #7; //half a wave is 5 time units
+          #5; //half a wave is 5 time units
           clk=1;//square wave is high
-          #7; //half a wave is 5 time units
+          #5; //half a wave is 5 time units
         end
     end
 	
 	initial begin //Start Output Thread
 	forever
          begin
-		 $display("(select:%2b)(Output:%4b)(regA:%2b)(data:%16b)",bb8.select, bb8.b, bb8.regA, bb8.Data0);
-		 #14;
+		 $display("(regA:%2b)(sum:%2b)(output:%4b)",bb8.regA, bb8.outputADD, bb8.opcode);
+		 #10;
 		 end
 	end
 	
 	initial begin//Start Stimulous Thread
-	#8
-	//select = 2'b00;
-	Data = 16'b0000111101011010;
-	#56
-	//select = 2'b01;
-	Data = 16'b0000111101011010;
-	#56
-	//select = 2'b10;
-	Data = 16'b0000111101011010;
-	#56
-	//select = 2'b11;
-	Data = 16'b0000111101011010;
-	#56
+	#6
+	rst=1;
+	data = 16'b0000111101011010;
+	#5
+	rst=0;
+	#30
+	data = 16'b1111111111111111;
+	#40
 	
 	$finish;
 	end
