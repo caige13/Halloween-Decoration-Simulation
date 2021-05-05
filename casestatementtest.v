f2477088ab6332d60a54cd5d0ba5d0a67b94ad5b
emilie-ko <61996789+emilie-ko@users.noreply.github.com>
2021-05-05 16:46:33 -0500

    Update casestatementtest.v

--- casestatementtest.v
+++ casestatementtest.v
@@ -10,6 +10,32 @@
 `define MOVEJAW   '4b1101 //11=Movement/Effect, 01=MOVEJAW
 `define FOG       '4b1110 //11=Movement/Effect, 10=FOG
 
+//////////////////////////////////
+module decoder_4x16 (d_out, d_in);
+
+   output [15:0] d_out;
+   input [3:0]   d_in;
+   reg d_out;
+   parameter tmp = 16'b0000_0000_0000_0001;
+always @ (d_in)
+	   d_out = (d_in == 4'b0000) ? tmp   :
+               (d_in == 4'b0001) ? tmp<<1:
+               (d_in == 4'b0010) ? tmp<<2:
+               (d_in == 4'b0011) ? tmp<<3:
+               (d_in == 4'b0100) ? tmp<<4:
+               (d_in == 4'b0101) ? tmp<<5:
+               (d_in == 4'b0110) ? tmp<<6:
+               (d_in == 4'b0111) ? tmp<<7:
+               (d_in == 4'b1000) ? tmp<<8:
+               (d_in == 4'b1001) ? tmp<<9:
+               (d_in == 4'b1010) ? tmp<<10:
+               (d_in == 4'b1011) ? tmp<<11:
+               (d_in == 4'b1100) ? tmp<<12:
+               (d_in == 4'b1101) ? tmp<<13:
+               (d_in == 4'b1110) ? tmp<<14:
+               (d_in == 4'b1111) ? tmp<<15: 16'bxxxx_xxxx_xxxx_xxxx;
+
+endmodule
 ///////////////////////////////////
 module OR_1(output Y, input A, B);
    assign Y = A | B; 
@@ -119,8 +145,8 @@ module testbench();
    reg rst;
 
     /*
-   always @(<>) begin
-	case(<decoder output>)
+   always @(d_out) begin
+	case(d_out)
 		16'b000000000000000 : initial begin
 						forever
 	 						begin
